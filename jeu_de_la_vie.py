d79cc1f0bc04bd1cafc23ec5253b24cb182195cc
uvsq22004516 <71332537+uvsq22004516@users.noreply.github.com>
2021-03-11 13:12:16 +0100

    Update jeu_de_la_vie.py

--- jeu_de_la_vie.py
+++ jeu_de_la_vie.py
@@ -12,15 +12,19 @@
 # import des modules
 
 import tkinter as tk
+import copy
 
 ##########################
 # constantes
 
 COUL_FOND = "grey20"
 COULEUR_QUADR = "grey60"
+COULEUR_VIVANT = "yellow"
 LARGEUR = 600
 HAUTEUR = 400
-COTE = 10
+COTE = 20
+NB_COL = LARGEUR // COTE
+NB_LIG = HAUTEUR // COTE
 
 #########################
 # variables globales
@@ -43,29 +47,54 @@ def quadrillage():
 
 
 
-def xy_to_cl(x, y):
+def xy_to_ij(x, y):
     """Retourne la colonne et la ligne du tableau correspondant aux coordonnées (x,y) du canevas"""
     # A FAIRE
-    return 0, 0
+    return x // COTE, y // COTE
 
 
 def chg_case(event):
     """Modifier l'état de la case aux coordonnées (event.x, event.y)"""
-    c, l = xy_to_cl(event.x, event.y)
-    if tableau[c][l] >= 0:
+    i, j = xy_to_ij(event.x, event.y)
+    if tableau[i][j] > 0:
         # si case vivante
-        canvas.delete(tableau[c][l])
-        tableau[c][l] = -1
+        canvas.delete(tableau[i][j])
+        tableau[i][j] = 0
     else:
         # si case morte
-        carre = canvas.create_rectangle((0,0),(COTE,COTE), fill=COULEUR_VIVANT)
-        tableau[c][l] = carre
+        x, y = COTE * i, COTE * j
+        carre = canvas.create_rectangle((x, y),(x + COTE, y + COTE), fill=COULEUR_VIVANT, outline=COULEUR_QUADR)
+        tableau[i][j] = carre
 
+
+def nb_vivant(i, j):
+    """Retourne le nombre de cases vivantes autour de la case de coordonnées (i, j)"""
+    pass
+
+
+def traite_case(i, j):
+    """Fait une étape de l'automate pour la case de coordonnées (i, j) 
+    en retournant la nouvelle valeur de la case du tableau"""
+    pass
+
+
+def etape():
+    """Fait une étape du jeu de la vie en modifiant la variable globale tableau"""
+    tableau_res = copy.deepcopy(tableau)
+    for i in range(NB_COL):
+        for j in range(NB_LIG):
+            traite_case(i, j)
+    tableau = tableau_res
 
 
 
 ########################
 # programme principal
+
+tableau = []
+for i in range(NB_COL):
+    tableau.append([0] * NB_LIG)
+
 
 racine = tk.Tk()
 racine.title("Jeu de la vie")
