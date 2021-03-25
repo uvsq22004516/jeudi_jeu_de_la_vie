81652baa96c5af98478d3961a7990e3dc561b487
uvsq22004516 <71332537+uvsq22004516@users.noreply.github.com>
2021-03-25 12:33:35 +0100

    Update jeu_de_la_vie.py

--- jeu_de_la_vie.py
+++ jeu_de_la_vie.py
@@ -69,23 +69,43 @@ def chg_case(event):
 
 def nb_vivant(i, j):
     """Retourne le nombre de cases vivantes autour de la case de coordonnées (i, j)"""
-    pass
+    cpt = 0
+    for k in range(max(0,i-1), min(NB_COL, i+2)):
+        for el in range(max(0,j-1), min(NB_LIG, j+2)):
+            if tableau[k][el] != 0 and [k, el] != [i, j]:
+                cpt += 1
+    return cpt
 
 
 def traite_case(i, j):
     """Fait une étape de l'automate pour la case de coordonnées (i, j) 
     en retournant la nouvelle valeur de la case du tableau"""
-    pass
+    n = nb_vivant(i, j)
+    if tableau[i][j] == 0:
+        # si case morte
+        if n == 3:
+            x, y = COTE * i, COTE * j
+            carre = canvas.create_rectangle((x, y),(x + COTE, y + COTE), fill=COULEUR_VIVANT, outline=COULEUR_QUADR)
+            return carre
+        else:
+            return 0           
+    else:
+        # si case vivante
+        if n in [2, 3]:
+            return tableau[i][j]
+        else:
+            canvas.delete(tableau[i][j])
+            return 0
 
 
-def etape():
+def etape(event):
     """Fait une étape du jeu de la vie en modifiant la variable globale tableau"""
+    global tableau
     tableau_res = copy.deepcopy(tableau)
     for i in range(NB_COL):
         for j in range(NB_LIG):
-            traite_case(i, j)
+            tableau_res[i][j] = traite_case(i, j)
     tableau = tableau_res
-
 
 
 ########################
@@ -106,6 +126,7 @@ canvas.grid()
 
 # gestion des événements
 canvas.bind("<Button-1>", chg_case)
+racine.bind("n", etape)
 
 # autres fonctions
 quadrillage()
